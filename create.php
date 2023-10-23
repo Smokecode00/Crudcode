f2d2873bbb9543f025f4bacdb5911b3bd6fb1740
SmokeCode00 <smokefacebook02@gmail.com>
2023-10-23 21:38:17 +0800

    Update create.php

--- create.php
+++ create.php
@@ -9,9 +9,37 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>CRUD</title>
 </head>
-<body>
-    <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #99B080;">
+<body style="background-color:#5C8374;">
+    <nav class="navbar navbar-light justify-content-center fs-3 mb-5" style="background-color: #99B080; font-weight:700;">
         Create, Read, Update, and Delete
     </nav>
+
+    <div class="container">
+        <div class="text-center mb-4">
+            <h3>Add New User</h3>
+            <p>Complete the form below to add a new user</p>
+        </div>
+
+        <div class="container d-flex justify-content-center">
+            <form action="" method="post" style="width:50vw; min-width:300px;">
+                <div class="row mb-2">
+                    <div class="col">
+                        <label class="form-label" style="font-weight:700;">First Name:</label>
+                        <input type="text" class="form-control" name="first_name" placeholder="Enter your firstname" required>
+                    </div>
+
+                    <div class="col">
+                        <label class="form-label" style="font-weight:700;">Last Name:</label>
+                        <input type="text" class="form-control" name="last_name" placeholder="Enter your lastname" required>
+                    </div>
+                </div>
+            </form>
+        </div>
+
+
+
+    </div>
+    <!-- Bootstrap Script -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
